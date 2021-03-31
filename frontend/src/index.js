ca24aa207ea2e35ab8553667c8eb6be93d9e9e76
QLewis <qplewis@asu.edu>
2021-03-30 22:11:53 -0700

    post lesson with Andrew

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,67 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
 function makeList() {
-  var mytext = document.createElement("p");
-  mytext.innerHTML = "I've been added to a list";
-  document.body.appendChild(mytext);
+  var myText = <p>"I've been added to a list"</p>
+  document.body.appendChild(myText);
 }
 
 const myelement = (
-  <button onClick={makeList}>Add an item!</button>
+  <button onClick={makeList}>
+    Add an item!
+  </button>
 )
+*/
+
+class ItemList extends React.Component {
+  state = {
+    items:[]
+  }
+
+  addThingToList = () => {
+    const strList = this.state.items;
+    strList.push(`I've been added to a list ${Math.random()}`);
+    this.setState({items: strList});
+    console.log('addThingToList', this.state.items);
+  }
+
+  renderItems = () => (
+    this.state.items.map(itm =>
+      <Item text={itm} /> 
+    )
+  );
+
+  render = () => (
+    <div>
+      <button onClick={this.addThingToList}>Add Thing To List</button>
+      {/*this.state.items.map(itm => <p key={itm}>{itm}</p>)*/}
+      {this.renderItems()}
+    </div>
+  )
+  
+}
+
+class Item extends React.Component {
+  state = {
+    color: 'green'
+  }
+
+  changeColor = () => {
+    console.log('changeColor')
+    if (this.state.color === 'green') {
+      this.setState({color: 'blue'});
+    } else {
+      this.setState({color: 'green'})
+    }
+  }
+
+  render = () => (
+    <div style={{background: this.state.color}} onClick={this.changeColor}>
+      {this.props.text}
+    </div>
+  )
+}
 
 /* ReactDOM.render(
   <React.StrictMode>
@@ -21,7 +73,11 @@ const myelement = (
   document.getElementById('root')
 ); */
 
-ReactDOM.render(myelement, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <ItemList />
+  </React.StrictMode>,
+document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
