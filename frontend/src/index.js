d13ef381965c230acb6f873363460b0f1a411440
QLewis <qplewis@asu.edu>
2021-04-01 09:48:47 -0700

    following tutorial

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,85 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-/*
-function makeList() {
-  var myText = <p>"I've been added to a list"</p>
-  document.body.appendChild(myText);
-}
 
 const myelement = (
-  <button onClick={makeList}>
-    Add an item!
-  </button>
-)
-*/
+  <div>
+    <h1>I am a Header.</h1>
+    <h1>I am a Header too.</h1>
+  </div>
+);
 
-class ItemList extends React.Component {
-  state = {
-    items:[]
-  }
 
-  addThingToList = () => {
-    const strList = this.state.items;
-    strList.push(`I've been added to a list ${Math.random()}`);
-    this.setState({items: strList});
-    console.log('addThingToList', this.state.items);
-  }
 
-  renderItems = () => (
-    this.state.items.map(itm =>
-      <Item text={itm} /> 
-    )
-  );
 
-  render = () => (
-    <div>
-      <button onClick={this.addThingToList}>Add Thing To List</button>
-      {/*this.state.items.map(itm => <p key={itm}>{itm}</p>)*/}
-      {this.renderItems()}
-    </div>
-  )
-  
-}
+ReactDOM.render(myelement, document.getElementById('root'));
 
-class Item extends React.Component {
-  state = {
-    color: 'green'
-  }
-
-  changeColor = () => {
-    console.log('changeColor')
-    if (this.state.color === 'green') {
-      this.setState({color: 'blue'});
-    } else {
-      this.setState({color: 'green'})
-    }
-  }
-
-  render = () => (
-    <div style={{background: this.state.color}} onClick={this.changeColor}>
-      {this.props.text}
-    </div>
-  )
-}
-
-/* ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-
-ReactDOM.render(
-  <React.StrictMode>
-    <ItemList />
-  </React.StrictMode>,
-document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
