78710d53407987cf8c550b0f6342933549fabe7d
QLewis <qplewis@asu.edu>
2021-04-04 15:36:43 -0700

    change text after button is clicked

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,85 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-/*
-function makeList() {
-  var myText = <p>"I've been added to a list"</p>
-  document.body.appendChild(myText);
-}
-
-const myelement = (
-  <button onClick={makeList}>
-    Add an item!
-  </button>
-)
-*/
-
-class ItemList extends React.Component {
-  state = {
-    items:[]
+class MyForm extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      inputText: '', //Text inside the input box
+      headerState: '', //Text of the header
+    };
   }
 
-  addThingToList = () => {
-    const strList = this.state.items;
-    strList.push(`I've been added to a list ${Math.random()}`);
-    this.setState({items: strList});
-    console.log('addThingToList', this.state.items);
+  changeText = (event) => {
+    this.setState({inputText: event.target.value}); //Grabs the text of the input box
   }
 
-  renderItems = () => (
-    this.state.items.map(itm =>
-      <Item text={itm} /> 
-    )
-  );
 
-  render = () => (
-    <div>
-      <button onClick={this.addThingToList}>Add Thing To List</button>
-      {/*this.state.items.map(itm => <p key={itm}>{itm}</p>)*/}
-      {this.renderItems()}
-    </div>
-  )
+  buttonChangeText = (event) => {
+    event.preventDefault(); //Stops page from refreshing after clicking the button
+    this.setState({headerState: this.state.inputText}); //Sets the text of the header
+  }
+  
+  render() {
+    return(
+      <form>
+        <h1>{this.state.headerState}</h1>
+        <p>Enter your text: </p>
+        <input onChange={this.changeText}/>
+        <button onClick={this.buttonChangeText}>Submit!</button>
+      </form>
+    );
+  }
   
 }
 
-class Item extends React.Component {
-  state = {
-    color: 'green'
-  }
 
-  changeColor = () => {
-    console.log('changeColor')
-    if (this.state.color === 'green') {
-      this.setState({color: 'blue'});
-    } else {
-      this.setState({color: 'green'})
-    }
-  }
-
-  render = () => (
-    <div style={{background: this.state.color}} onClick={this.changeColor}>
-      {this.props.text}
-    </div>
-  )
-}
-
-/* ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-
-ReactDOM.render(
-  <React.StrictMode>
-    <ItemList />
-  </React.StrictMode>,
-document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<MyForm/>, document.getElementById('root'));
