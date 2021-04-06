9eb1e4538852411111d51d3a6aa9754858d44a9e
QLewis <qplewis@asu.edu>
2021-04-06 09:13:15 -0700

    learned about setting state in intervals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Football extends React.Component {
-  shoot(a) {
-    alert(a);
+class Clock extends React.Component {
+  constructor() {
+    super();
+    //Initialize the date to show the current date and time
+    this.state = {date: new Date()};
   }
+
+  //Update the state to the current date and time
+  tick() {
+    this.setState({date: new Date()});
+  }
+
+  //After rendering, set a timer to call the call the tick() function every second
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
   render() {
+    //Show only the current time
     return (
-      <button
-        onClick={this.shoot(this, "Goal")}
-      >Take the shot!</button>
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<Football />, document.getElementById('root'));
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
 
