031231ea946b286c6061a5928215b02aad25dc66
Rebellia <sleepy.wrath@gmail.com>
2021-04-15 22:07:09 -0700

    updating array of movies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,38 +3,90 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class ItemList extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     //Initialize a state with an empty array
     this.state = {
-      items: [], //array of text
-      inputText: '', //text from the input box
+     /*  title: '',
+      genre: '',
+      date: '', */
+      movies: [{
+        title: '',
+        genre: '',
+        date: ''
+      }],
+      titleText: '',
+      genreText: '',
+      dateText: '',
     };
   }
-
-  changeText = (event) => {
-    this.setState({inputText: event.target.value}); //Grabs the text of the input box
-  }
-
-  addThingToList = (event) => {
-    event.preventDefault(); //Stops page from refreshing after clicking the button
-    var strList = this.state.items; //grabs the array
-    strList.push(this.state.inputText); //adds string to the array
-    this.setState({items: strList}); //set the state of the array to the current array
-  }
-
-  //For every item in the array, return a paragraph element with that item in it
-  renderItems = () => (
-    this.state.items.map(itm => <p>{itm}</p>)
-  );
   
+  //Change and set the title
+  changeTitle = (event) => {
+    this.setState({titleText: event.target.value}); //Grabs the text of the input box
+  }
+
+  setTitle = (event) => {
+    event.preventDefault(); //Stops page from refreshing after clicking the button
+    this.setState({title: this.state.titleText}); //Grabs the text of the input box
+  }
+
+  //Change and set the genre
+  changeGenre = (event) => {
+    this.setState({genreText: event.target.value}); //Grabs the text of the input box
+  }
+
+  setGenre = (event) => {
+    event.preventDefault(); //Stops page from refreshing after clicking the button
+    this.setState({genre: this.state.genreText}); //Grabs the text of the input box
+  }
+
+  //Change and set the date
+  changeDate = (event) => {
+    this.setState({dateText: event.target.value}); //Grabs the text of the input box
+  }
+
+  setDate = (event) => {
+    event.preventDefault(); //Stops page from refreshing after clicking the button
+    this.setState({date: this.state.dateText}); //Grabs the text of the input box
+  }
+
+
+  addMovieToList = (event) => {
+    event.preventDefault();
+    var newMovie = [{
+      title: this.state.titleText,
+      genre: this.state.genreText,
+      date: this.state.genreText
+    }];
+
+    var movieList = this.state.movies;
+    movieList.push(newMovie);
+    this.setState({movies: movieList});
+
+    console.log(this.state.movies);
+  }
+
+  
+  renderMovies = () => {
+    <p>My new movie</p>
+  }
+
   render() {
     return(
       <form>
-        <p>Enter your text:</p>
-        <input onChange={this.changeText}/>
-        <button onClick={this.addThingToList}>Add Item</button>
-        {this.renderItems()}
+        <p>What is the title?</p>
+        <input onChange={this.changeTitle}/>
+        <br/>
+        <p>What is the genre?</p>
+        <input onChange={this.changeGenre}/>
+        <br/>
+        <p>What is the date?</p>
+        <input onChange={this.changeDate}/>
+        <br/>
+        <br/>
+        <button onClick={this.addMovieToList}>Add Movie</button>
+        {this.renderMovies()}
       </form>
     );
   }
