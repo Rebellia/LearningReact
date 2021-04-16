47fd2d20d8841c55724018efa8de62597252fe1c
Rebellia <sleepy.wrath@gmail.com>
2021-04-15 23:22:17 -0700

    rendering array of movies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -7,10 +7,8 @@ class ItemList extends React.Component {
     super(props);
     //Initialize a state with an empty array
     this.state = {
-     /*  title: '',
-      genre: '',
-      date: '', */
       movies: [{
+        key: '',
         title: '',
         genre: '',
         date: ''
@@ -54,40 +52,70 @@ class ItemList extends React.Component {
 
   addMovieToList = (event) => {
     event.preventDefault();
-    var newMovie = [{
+    var newMovie = {
+      key: Math.random(),
       title: this.state.titleText,
       genre: this.state.genreText,
       date: this.state.genreText
-    }];
+    };
 
     var movieList = this.state.movies;
     movieList.push(newMovie);
     this.setState({movies: movieList});
-
-    console.log(this.state.movies);
+    //console.log(this.state.movies);
   }
 
   
   renderMovies = () => {
-    <p>My new movie</p>
+    const myMovies = this.state.movies;
+
+    /* const mapRows = myMovies.map((movie) => (
+      <Fragment key={movie.key}>
+        <li>
+          <h1>Title: {movie.title}</h1>
+          <p>Genre: {movie.genre}</p>
+        </li>
+      </Fragment>
+    ));
+    
+    return mapRows;   */
+
+    {myMovies.map((movie, key) => (
+      <li key={key}>
+        <p>{movie.title}</p>
+      </li>
+    ))}
   }
+    
 
   render() {
+    console.log(this.state.movies);
     return(
-      <form>
-        <p>What is the title?</p>
-        <input onChange={this.changeTitle}/>
-        <br/>
-        <p>What is the genre?</p>
-        <input onChange={this.changeGenre}/>
-        <br/>
-        <p>What is the date?</p>
-        <input onChange={this.changeDate}/>
-        <br/>
-        <br/>
-        <button onClick={this.addMovieToList}>Add Movie</button>
-        {this.renderMovies()}
-      </form>
+      <>
+        <form>
+          <p>What is the title?</p>
+          <input onChange={this.changeTitle}/>
+          <br/>
+          <p>What is the genre?</p>
+          <input onChange={this.changeGenre}/>
+          <br/>
+          <p>What is the date?</p>
+          <input onChange={this.changeDate}/>
+          <br/>
+          <br/>
+          <button onClick={this.addMovieToList}>Add Movie</button>
+        </form>
+
+        <div>
+          {this.state.movies.map(movie => (
+            <div key={movie.key}>
+              <h1>Title: {movie.title}</h1>
+              <p>Genre: {movie.genre}</p>
+            </div>
+          ))}
+        </div>
+      </>
+      
     );
   }
 }
