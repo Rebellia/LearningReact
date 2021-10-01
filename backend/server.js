7653803cace21c7fbe98c209b9da93837b0c9d55
Rebellia <sleepy.wrath@gmail.com>
2021-10-01 15:17:41 -0700

    initial author routes

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@
 const express = require("express")
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 //import router into server
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 //Configure the express application
 app.set('view engine', 'ejs') //set the view engine to ejs
@@ -15,7 +17,7 @@ app.set('views', __dirname + '/views') //set where the server-rendered views wil
 app.set('layout', 'layouts/layout') //set what the layout file will be
 app.use(expressLayouts) //tell express application to use express layouts
 app.use(express.static('public')) //tell express where the public files are
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 //Connect to database
 const mongoose = require('mongoose')
@@ -35,6 +37,7 @@ connection.once("open", function() {
 
 //use the indexRouter at the root of our project
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 //listen on a certain port, either an environment variable, or default 3000
 app.listen(process.env.PORT || 3000)
